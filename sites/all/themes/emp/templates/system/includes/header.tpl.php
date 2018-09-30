14aba0fb626046571af9aa5d0b98cbb8600bfdfa
narendradesai <shenvidesainarendra@gmail.com>
2018-09-30 18:52:29 +0530

    Menu changes in header

--- sites/all/themes/emp/templates/system/includes/header.tpl.php
+++ sites/all/themes/emp/templates/system/includes/header.tpl.php
@@ -1,14 +1,17 @@
 <div class="header">
     <ul class="">
         <li class="padding-0-5-rem"><a href="#">Logo</a></li>
-        <li class="">
-            <ul class="menu-bar">
-                <?php echo drupal_render(menu_tree('main-menu')); ?>
-                <!-- <li class="padding-0-5-rem"><a href="#">Home</a></li>
-                <li class="padding-0-5-rem"><a href="#">Services</a></li>
-                <li class="padding-0-5-rem"><a href="#">About Us</a></li>
-                <li class="padding-0-5-rem"><a href="#">Contact Us</a></li> -->
-            </ul>
+        <li class="main-menu">
+            <div class="menu-bar-button padding-0-5-rem">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <?php echo drupal_render(menu_tree('main-menu')); ?>
+            <!-- <li class="padding-0-5-rem"><a href="#">Home</a></li>
+            <li class="padding-0-5-rem"><a href="#">Services</a></li>
+            <li class="padding-0-5-rem"><a href="#">About Us</a></li>
+            <li class="padding-0-5-rem"><a href="#">Contact Us</a></li> -->
         </li>
     </ul>
 </div>
